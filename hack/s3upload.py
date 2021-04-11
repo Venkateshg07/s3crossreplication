8bd6bdd996c15617c6385a780359e572d7e9c858
Venkateshg07 <gaddamv@outlook.com>
2021-04-11 14:02:42 +0100

    updated script

--- hack/s3upload.py
+++ hack/s3upload.py
@@ -5,17 +5,26 @@ import sys
 from boto3.s3.transfer import TransferConfig
 BUCKET_NAME = "veerumassign20210410162734799500000002"
 
+buckets_name_with_key = {
+    "bc71608c-8b6f-499c-bd3f-8c322408636a" : ["log1a.veerum", "log1b.veerum"],
+    "87c11788-df54-4dc1-acc4-09d66fb64b66" : ["log2a.veerum", "log2b.veerum"]
+}
+
 s3 = boto3.resource('s3')
 
 def multi_part_upload_with_s3():
     config = TransferConfig(multipart_threshold=1024 * 25, max_concurrency=10,
                             multipart_chunksize=1024 * 25, use_threads=True)
-    file_path = 'largefile.pdf'
-    key_path = 'largefile.pdf'
-
-    s3.meta.client.upload_file(file_path, BUCKET_NAME, key_path,
-                            ExtraArgs={ "ServerSideEncryption": "aws:kms", "SSEKMSKeyId":"9956e54b-7420-4914-8cb4-ab18ad79f213" },
+    for kms, buckets in buckets_name.items():
+        for bucket in buckets:
+            file_path = '%s.pdf' %(bucket)
+            key_path =  '%s.pdf' %(bucket)
+            print(file_path)
+            pirnt(key_path)
+            s3.meta.client.upload_file(file_path, BUCKET_NAME, key_path,
+                            ExtraArgs={ "ServerSideEncryption": "aws:kms", "SSEKMSKeyId": kms },
                              Config=config
                             )
 if __name__ == '__main__': 
     multi_part_upload_with_s3()
+
